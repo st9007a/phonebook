6a03b9efbf739257c27c594167d6b92e0d34a37a
uno <st9007a@gmail.com>
2017-09-24 16:31:59 +0800

    Reduce the size of __PHONEBOOK_ENTRY

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -6,8 +6,7 @@
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
 // #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+typedef struct __PHONE_BOOK_INFO {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,6 +16,11 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} info;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __PHONE_BOOK_INFO *pInfo;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
