5a35302efa59de8908280eb91bff96bc089cedb4
uno <st9007a@gmail.com>
2017-10-16 12:08:22 +0800

    Remove dead code

--- phonebook.h
+++ phonebook.h
@@ -2,11 +2,7 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 16
-// #define HASH_TABLE_SIZE 499857
-// #define HASH_TABLE_SIZE 465533
 #define HASH_TABLE_SIZE 437375
-// #define HASH_TABLE_SIZE 388777
-// #define HASH_TABLE_SIZE 350000
 typedef struct __PHONE_BOOK_INFO info;
 typedef struct __PHONE_BOOK_ENTRY entry;
 typedef struct __HASH_TABLE hashTable;
