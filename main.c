3dca2a0a07a8d7d578a711ff7adfa64489d15c4d
uno <st9007a@gmail.com>
2017-10-17 20:18:03 +0800

    Remove dead code

--- main.c
+++ main.c
@@ -65,11 +65,8 @@ int main(int argc, char *argv[])
     /* close file as soon as possible */
     fclose(fp);
 
-    /* e = pHead; */
-
     /* the givn last name to find */
     char input[MAX_LAST_NAME_SIZE] = "zyxel";
-    /* e = pHead; */
 
     assert(findName(input, table) &&
            "Did you implement findName() in ' phonebook.h '?");
@@ -90,9 +87,6 @@ int main(int argc, char *argv[])
 
     printf("execution time of append() : %lf sec\n", cpu_time1);
     printf("execution time of findName() : %lf sec\n", cpu_time2);
-
-    /* if (pHead->pNext) free(pHead->pNext); */
-    /* free(pHead); */
 
     return 0;
 }
